0376cbbee98e97ffb16c71798d355604a6db7183
Igor Cardoso <iygorsilva@gmail.com>
2021-11-18 00:06:34 -0300

    actions creation and change in the service in the front

--- backend-store/app/controllers/products_controller.rb
+++ backend-store/app/controllers/products_controller.rb
@@ -1,8 +1,27 @@
 class ProductsController < ApplicationController
     def show
         @products = Product.all
-        puts @products
-        
         render json: @products
+    end
+
+    def create
+        @products = Product.new(product_params)
+    
+        if @products.save
+            render json: @products, status: :created
+        else
+            render json: @products.erros, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
+    def product_params
+        params.require(:produto).permit(:title, :price)
+        # ActiveModelSerializers::Deserialization.jsonapi_parse(params, polymorphic: [:user], embedded: [:user_detail])
     end
 end
