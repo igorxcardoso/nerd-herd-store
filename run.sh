53f05e9021fa44592f8f7f61f7343c4c423a876d
Igor Cardoso <iygorsilva@gmail.com>
2021-11-19 16:16:47 -0300

    script creation to automate startup

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Running the project..."
+
+cd backend-store && rails s
+
+
+# if ![ cd backend-store && exec 'rails s' ]; then
+# echo "Falaha em subir o back"
+
+# cd backend-store
+
+# exit 1
