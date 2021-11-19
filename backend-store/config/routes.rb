83652711cd4f259eb24ffd084ceba22a576d13f0
Igor Cardoso <iygorsilva@gmail.com>
2021-11-19 16:19:34 -0300

    creation of routes to: find product by id, delete and update by id

--- backend-store/config/routes.rb
+++ backend-store/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resource :products
+  get 'products/:id', to: 'products#findByid'
+  delete 'products/:id', to: 'products#deleteByid'
+  put 'products/:id', to: 'products#updateByid'
+
 end
