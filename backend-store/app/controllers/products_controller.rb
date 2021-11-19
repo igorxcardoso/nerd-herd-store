1a376fcb940c7b5289f6c6727ee681e598bed2ac
Igor Cardoso <iygorsilva@gmail.com>
2021-11-19 16:20:32 -0300

    actions for routes

--- backend-store/app/controllers/products_controller.rb
+++ backend-store/app/controllers/products_controller.rb
@@ -5,21 +5,46 @@ class ProductsController < ApplicationController
     end
 
     def create
-        @products = Product.new(product_params)
+        @product = Product.new(product_params)
     
-        if @products.save
-            render json: @products, status: :created
+        if @product.save
+            render json: @product, status: :created
         else
-            render json: @products.erros, status: :unprocessable_entity
+            render json: @product.erros, status: :unprocessable_entity
         end
     end
 
     def destroy
     end
 
-    def update
+    def deleteByid  # Vai deletar apartir do id
+        Product.find(params[:id]).destroy!
+        head :no_content
     end
 
+    def findByid    # Vai buscar e retornar para o front o produto pelo id
+        @product = Product.find(params[:id])
+        render json: @product, status: :accepted
+    end
+
+    def update
+        # if @product.update(product_params)
+        #     render json: @product, status: :created
+        # else
+        #     render json: @product.erros, status: :unprocessable_entity
+        # end
+    end
+
+    def updateByid
+        puts @product
+        # if @product.update(product_params)
+        #     render json: @product, status: :created
+        # else
+        #     render json: @product.erros, status: :unprocessable_entity
+        # end
+    end
+
+    private
     def product_params
         params.require(:produto).permit(:title, :price)
         # ActiveModelSerializers::Deserialization.jsonapi_parse(params, polymorphic: [:user], embedded: [:user_detail])
