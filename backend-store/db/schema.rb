02a86a75fb496e422f6f88439c48d729254c6591
Igor Cardoso <iygorsilva@gmail.com>
2021-12-06 10:28:12 -0300

    database change for PostgreSQL

--- backend-store/db/schema.rb
+++ backend-store/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_17_195739) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.float "price"
